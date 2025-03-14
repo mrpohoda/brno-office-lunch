127a4a17b757e9ca193b875b5078cd65f94fc280
Jan Liška <mr.pohoda@gmail.com>
2025-03-14 12:51:00 +0100

    fixed cheerio

--- components/borgeska.jsx
+++ components/borgeska.jsx
@@ -1,4 +1,4 @@
-import cheerio from "cheerio";
+import * as cheerio from 'cheerio';
 import {loadData} from "../utils/load";
 import {markTomato} from "../utils/tomato";
 import {normalize} from "../utils/normalize";
--- components/opice.jsx
+++ components/opice.jsx
@@ -1,4 +1,4 @@
-import cheerio from "cheerio";
+import * as cheerio from 'cheerio';
 import {loadData} from "../utils/load";
 import {decode} from "iconv-lite";
 import {markTomato} from "../utils/tomato";
